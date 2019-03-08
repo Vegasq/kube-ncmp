d8ba01d749f64731589c315e81bd6d7aecf32dab
Mykola Yakovliev <VegasQ@gmail.com>
2019-03-08 13:02:45 -0600

    Build script changes

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-cp -r ~/.kube/ .
-docker build . -t kube-ncmp
+cp kube-ncmp.py prometheus-kube-network-metrics
+helm install --name prometheus-kube-network-metrics ./prometheus-kube-network-metrics --namespace=osh-infra
+
