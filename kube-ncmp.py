5729f32f9a0ea6112bc63a79d0d72a1a8673706a
Mykola Yakovliev <VegasQ@gmail.com>
2019-03-12 13:21:21 -0500

    Rework for community

--- kube-ncmp.py
+++ kube-ncmp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Copyright 2019 AT&T Intellectual Property. All other rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +24,7 @@ from kubernetes.config import config_exception
 from kubernetes.client import configuration
 from kubernetes.stream import stream
 
-from prometheus_client import start_http_server, Enum
+from prometheus_client import start_http_server, Gauge
 from prometheus_client.core import REGISTRY
 from prometheus_client.process_collector import PROCESS_COLLECTOR
 from prometheus_client.platform_collector import PLATFORM_COLLECTOR
@@ -37,83 +37,113 @@ ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
 
-class Config(dict):
-    def __getattr__(self, key):
-        return self[key]
+def ip_to_prefix(ip):
+    return ".".join(ip.split(".")[:2] + ["x", "x"])
 
-    def __setattr__(self, key, value):
-        self[key] = value
+
+class Config:
+    def set_args(self, args):
+        self._args = args
+
+    @property
+    def namespace(self):
+        return self._args.namespace
+
+    @property
+    def filter(self):
+        return self._args.filter
+
+    @property
+    def port(self):
+        return self._args.port
+
+    @property
+    def use_cache(self):
+        return self._args.cache
+
+    @property
+    def sleep(self):
+        return self._args.sleep
+
+    @property
+    def show_system_info(self):
+        return self._args.show_system_info
 
 
 CONFIG = Config()
 
 
-def ip_to_subnet(ip):
-    return ".".join(ip.split(".")[:2] + ["x", "x"])
+class Container:
+    def __init__(self, host, pod, container, ip):
+        self.host = host
+        self.pod = pod
+        self.container = container
+        self.ip = ip
+
+    def __str__(self):
+        return "[{host}/{pod}/{container}/{ip}]".format(
+            host=self.host, pod=self.pod, container=self.container, ip=self.ip)
 
 
 class Report:
-    OK = "OK"
-    FAIL = "FAIL"
-    UNKNOWN = "UNKNOWN"
+    OK = 0
+    FAIL = 1
 
-    def __init__(self, port=8000):
-        self.enum_state = Enum(
+    def __init__(self):
+        self.state = Gauge(
             "network_state",
             "Current state of underlay networking",
-            states=[self.UNKNOWN, self.OK, self.FAIL],
             labelnames=['host_from', 'host_to', 'namespace', 'network']
         )
         if not CONFIG.show_system_info:
             REGISTRY.unregister(PROCESS_COLLECTOR)
             REGISTRY.unregister(PLATFORM_COLLECTOR)
 
-        start_http_server(port)
+        start_http_server(CONFIG.port)
 
-    def ok(self, host_from, host_to, namespace, network):
-        self.enum_state.labels(
+    def ok(self, host_from, host_to, network):
+        self.state.labels(
             host_from=host_from,
             host_to=host_to,
-            namespace=namespace,
-            network=network).state(self.OK)
+            namespace=CONFIG.namespace,
+            network=network).set(self.OK)
 
-    def fail(self, host_from, host_to, namespace, network):
-        self.enum_state.labels(
+    def fail(self, host_from, host_to, network):
+        self.state.labels(
             host_from=host_from,
             host_to=host_to,
-            namespace=namespace,
-            network=network).state(self.FAIL)
+            namespace=CONFIG.namespace,
+            network=network).set(self.FAIL)
 
 
 class Cache:
     ping_pods_cache_path = "/var/log/%s_ping_pods_cache"
 
     @classmethod
-    def load(cls, namespace):
-        cls.ping_pods_cache = cls.ping_pods_cache_path % namespace
+    def load(cls):
+        cls.ping_pods_cache = cls.ping_pods_cache_path % CONFIG.namespace
 
         try:
             with open(cls.ping_pods_cache, "r") as fl:
-                return json.loads(fl.read())
+                data = json.loads(fl.read())
+                return [Container(*d) for d in data]
         except IOError:
             logger.error("No ping_pods cache found")
         return None
 
     @classmethod
-    def save(cls, namespace, pods):
-        cls.ping_pods_cache = cls.ping_pods_cache_path % namespace
+    def save(cls, pods):
+        cls.ping_pods_cache = cls.ping_pods_cache_path % CONFIG.namespace
+        dump_me = [(p.host, p.pod, p.container, p.ip) for p in pods]
         with open(cls.ping_pods_cache, "w") as fl:
-            fl.write(json.dumps(pods))
+            fl.write(json.dumps(dump_me))
 
 
 class NCMashedPotato:
-    SUCCESS = "Success"
+    OK = "Success"
     FAIL = "Fail"
 
-    def __init__(self, namespace, filter, port, use_cache=True):
-        self.filter = filter or None
-        self.namespace = namespace
-
+    def __init__(self):
         conf = client.Configuration()
         conf.verify_ssl = False
 
@@ -127,187 +157,150 @@ class NCMashedPotato:
         self.api = client.CoreV1Api()
 
         # To communicate with Prometeus
-        self.report = Report(port)
-        self.pods_in_nodes = self._collect_all_namespaced_pods()
-        self.ping_pods = self._select_only_nodes_with_ping(use_cache=use_cache)
+        self.report = Report()
+        self._containers = self._collect_all_containers()
+        self._ping_containers = self._collect_containers_with_ping()
 
-    def _collect_all_namespaced_pods(self):
-        """
-        return: {"hostname": [hostname, pod_name, pod_ip]...}
-        """
-        os_pods = self.api.list_namespaced_pod(self.namespace).items
-        if self.filter:
-            os_pods = [p for p in os_pods if self.filter in p.metadata.name]
-        pods_in_nodes = {}
+    def _collect_all_containers(self):
+        os_pods = self.api.list_namespaced_pod(CONFIG.namespace).items
+        if CONFIG.filter:
+            os_pods = [p for p in os_pods if CONFIG.filter in p.metadata.name]
+
+        containers = []
 
         for pod in os_pods:
             host_name = pod.spec.node_name
-            # TODO(Mykola): We select only first container within pod,
+            # TODO(vegasq): We select only first container within pod,
             #               should we use all of them?
             container_name = pod.spec.containers[0].name
 
-            if host_name not in pods_in_nodes:
-                pods_in_nodes[host_name] = []
-
             if pod.status.container_statuses[0].state.running:
-                pods_in_nodes[host_name].append(
-                    [
-                        host_name,
-                        pod.metadata.name,
-                        container_name,
-                        pod.status.pod_ip,
-                    ]
-                )
+                cnt = Container(
+                    host=host_name, pod=pod.metadata.name,
+                    container=container_name, ip=pod.status.pod_ip)
+                containers.append(cnt)
 
-        return pods_in_nodes
+        return containers
 
-    def _select_only_nodes_with_ping(self, use_cache=False):
-        """Go over all pods and collect ones with ping util."""
+    def _collect_containers_with_ping(self):
+        """Go over all containers and collect ones with ping util."""
         logger.debug("Collecting pods with ping utility.")
 
-        if use_cache:
-            pods = Cache.load(self.namespace)
+        if CONFIG.use_cache:
+            pods = Cache.load()
             if pods:
                 return pods
 
-        ping_pods = {}
+        ping_pods = []
 
-        for hostname in self.pods_in_nodes:
-            if hostname not in ping_pods:
-                ping_pods[hostname] = []
+        for container in self._containers:
+            try:
+                resp = self.connect_get_namespaced_pod_exec(
+                    container, ["/bin/sh", "-c", "ping"])
+                if "Usage: ping" in resp:
+                    logger.debug("Ping found at %s" % container)
+                    ping_pods.append(container)
+            except Exception as err:
+                logger.error(err)
 
-            for pod in self.pods_in_nodes[hostname]:
-                try:
-                    resp = self.connect_get_namespaced_pod_exec(
-                        pod[1], pod[2], ["/bin/sh", "-c", "ping"]
-                    )
-                    if "not found" not in resp:
-                        logger.debug("Ping found at %s" % pod)
-                        ping_pods[hostname].append(pod)
-                except Exception as err:
-                    logger.error(err)
-
-        # We will save it even when use_cache == False, so we can use it next
-        # if we want to.
-        Cache.save(self.namespace, ping_pods)
+        Cache.save(ping_pods)
         return ping_pods
 
-    def connect_get_namespaced_pod_exec(self, pod_name, container_name, cmd):
+    def connect_get_namespaced_pod_exec(self, container, cmd):
         return stream(
             self.api.connect_get_namespaced_pod_exec,
-            pod_name,
-            self.namespace,
+            container.pod,
+            CONFIG.namespace,
             command=cmd,
-            container=container_name,
+            container=container.container,
             stderr=True,
             stdin=False,
             stdout=True,
             tty=False,
         )
 
-    def _pods_on_different_nodes(self, pod_ip):
-        """Look for a single pod per node that have same subnet."""
+    def _containers_on_different_nodes(self, container):
+        """Look for a containers in same networks on a variety of hosts."""
         yielded_hosts = []
-        for name in self.pods_in_nodes:
-            for pod in self.pods_in_nodes[name]:
-                if name in yielded_hosts:
-                    continue
-                if ip_to_subnet(pod_ip) == ip_to_subnet(pod[3]):
-                    # We need ony one pod per node, so we track already found
-                    # nodes.
-                    yielded_hosts.append(name)
-                    yield pod
+        for remote_container in self._containers:
+            if remote_container.host in yielded_hosts:
+                continue
+            if ip_to_prefix(container.ip) == ip_to_prefix(remote_container.ip):
+                # We need ony one pod per node, so we track already found
+                # nodes.
+                yielded_hosts.append(remote_container.host)
+                yield remote_container
 
-    def check_connection(self, name, container, ip):
+    def _check_connection(self, cnt_a, cnt_b):
         """Use K8S API method connect_get_namespaced_pod_exec to ping
         specifyed IP.
-
-        Only output with `0% packet loss` in it recognized as successful.
         """
-        exec_command = ["/bin/sh", "-c", "ping -c 2 %s" % ip]
+        exec_command = ["/bin/sh", "-c", "ping -c 2 %s" % cnt_b.ip]
+
         try:
-            resp = self.connect_get_namespaced_pod_exec(
-                name, container, exec_command
-            )
+            resp = self.connect_get_namespaced_pod_exec(cnt_a, exec_command)
 
             logger.debug("Response: " + resp)
             if "0% packet loss" in resp:
-                return self.SUCCESS
+                return self.OK
         except Exception as err:
             logger.error(err)
 
-        # Report Fail before end of validation since we already knwo the result
         return self.FAIL
 
     def _generate_report_tempalte(self):
         """For every loop cycle, create new dict of hosts."""
-        hosts = self.pods_in_nodes.keys()
+        hosts = set([cnt.host for cnt in self._containers])
         connectivity_status = {}
         for host in hosts:
             connectivity_status[host] = {h: {} for h in hosts}
         return connectivity_status
 
-    def _validate_connection_between(self, pod_a, pod_b):
+    def _validate_connection_between(self, cnt_a, cnt_b):
         """Check connectivity between two nodes if state between nodes
-        is unknown, and update connection status report.
-
-        If connectivity is broken, report failure immediately.
+        is unknown, and report status.
         """
-        sub = ip_to_subnet(pod_a[3])
-        if sub not in self.connectivity_status[pod_a[0]][pod_b[0]]:
-            self.connectivity_status[pod_a[0]][pod_b[0]][sub] = {}
+        sub = ip_to_prefix(cnt_a.ip)
+        if sub not in self.connectivity_status[cnt_a.host][cnt_b.host]:
+            self.connectivity_status[cnt_a.host][cnt_b.host][sub] = None
 
-        # We already had one successful attempt, underlay network
-        # is working
-        if self.connectivity_status[pod_a[0]][pod_b[0]][sub] == self.SUCCESS:
+        # We already had one successful attempt, underlay network is working
+        if self.connectivity_status[cnt_a.host][cnt_b.host][sub] == self.OK:
             return
         else:
-            st = self.check_connection(pod_a[1], pod_a[2], pod_b[3])
-            print(
-                "Connection between %s and %s via %s is %s."
-                % (pod_a, pod_b, sub, st)
-            )
+            st = self._check_connection(cnt_a, cnt_b)
+            print("Connection between %s and %s via %s is %s."
+                  % (cnt_a, cnt_b, sub, st))
 
-            if st == self.SUCCESS:
-                f = self.report.ok
-            else:
-                f = self.report.fail
-
-            f(host_from=pod_a[0], host_to=pod_b[0],
-              namespace=self.namespace, network=sub)
+            # Report state to Prometheus
+            f = self.report.ok if st == self.OK else self.report.fail
+            f(host_from=cnt_a.host, host_to=cnt_b.host, network=sub)
 
             # Keep result to not report multiple times.
-            self.connectivity_status[pod_a[0]][pod_b[0]][sub] = st
+            self.connectivity_status[cnt_a.host][cnt_b.host][sub] = st
 
     def _validate(self):
         """Start validation cycle."""
         self.connectivity_status = self._generate_report_tempalte()
 
-        for ping_host in self.ping_pods:
-            for ping_pod in self.ping_pods[ping_host]:
-                for remote_pod in self._pods_on_different_nodes(ping_pod[3]):
-                    logger.debug(
-                        "Check connectivity between differen nodes: "
-                        "%s -> %s" % (ping_pod, remote_pod)
-                    )
-                    # It will update self.connectivity_status
-                    self._validate_connection_between(ping_pod, remote_pod)
+        for cnt in self._ping_containers:
+            for remote_cnt in self._containers_on_different_nodes(cnt):
+                logger.debug(
+                    "Check connectivity between differen nodes: "
+                    "%s -> %s" % (cnt, remote_cnt))
+                self._validate_connection_between(cnt, remote_cnt)
 
-        # TODO(Mykola): is self.DEBUG ?
         pprint(self.connectivity_status)
 
-        return self.connectivity_status
-
-    def start_validation(self, sleep_for):
-        """Infinite loop"""
+    def start_validation(self):
         logger.debug("Start infinity loop.")
         while True:
             logger.debug("Start validation.")
             self._validate()
-            time.sleep(sleep_for)
+            time.sleep(CONFIG.sleep)
 
 
-def main():
+def parse_args_to_config():
     parser = argparse.ArgumentParser(
         prog="NC Mashed Potato",
         description="Tool to validate interconnection between containers "
@@ -337,16 +330,12 @@ def main():
         action="store_true",
         help="Enable process and platform collectors."
     )
-    args = parser.parse_args()
+    CONFIG.set_args(parser.parse_args())
 
-    CONFIG.show_system_info = args.show_system_info
 
-    NCMashedPotato(
-        namespace=args.namespace,
-        filter=args.filter,
-        port=args.port,
-        use_cache=args.cache,
-    ).start_validation(args.sleep)
+def main():
+    parse_args_to_config()
+    NCMashedPotato().start_validation()
 
 
 if __name__ == "__main__":
