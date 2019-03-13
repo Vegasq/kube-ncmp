23313ddbfe2fe1acab9a9fed6f554d327edd19b3
Mykola Yakovliev <VegasQ@gmail.com>
2019-03-13 14:33:56 -0500

    Credits

--- kube_ncmp.py
+++ kube_ncmp.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # Copyright 2019 AT&T Intellectual Property. All other rights reserved.
 #
+# Author: Mykola Yakovliev <vegasq@gmail.com>
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
